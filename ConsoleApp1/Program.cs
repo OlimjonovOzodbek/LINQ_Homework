bc4f43638d072a1d117ee53e2a0e0a6f9b142aa9
Ozodbek <olimjanovozodbek32@gmail.com>
2024-02-20 19:50:42 +0500

    Tugadi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,11 +4,6 @@
     {
         static void Main(string[] args)
         {
-            //foreach (var i in Methods.GetWorker()) 
-            //{ 
-            //    Console.WriteLine($"ID {i.id}\nNAME {i.name}");
-            //    Console.WriteLine();
-            //}
             Methods.WorkerList();
         }
     }
